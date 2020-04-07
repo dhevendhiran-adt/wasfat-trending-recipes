d8fb46a4bc4a164bc500ededf1d0a5eea84d2791
Apple <dhevendhiran-adt@altradimension.com>
2020-04-07 19:57:34 +0530

    wasfat push testing

--- src/components/home/title.js
+++ src/components/home/title.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Title() {
     return (
         <div className='title-container'>
-            <h1>Wasfat</h1>
+            <h1>Wasfat Updated</h1>
             <h2>Trending Recipe</h2>
         </div>
     )
